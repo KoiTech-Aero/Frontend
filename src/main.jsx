32534e56e2add1857e6d1a63ae1d0d0a08d3ae78
guilherme <guilherme.alvarenga021@gmail.com>
2026-03-22 08:41:07 -0300

    fix: layout padrão + correção nas rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import PesquisarNorma from './pages/PesquisarNorma.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/pesquisarNorma",
-    element: <PesquisarNorma />
-  }
-])
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <App />
+  </StrictMode>
 )
