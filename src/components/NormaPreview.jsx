0040002c26671ca63dd278006a77dbd1112e075c
guilherme <guilherme.alvarenga021@gmail.com>
2026-03-30 19:52:51 -0300

    feat: exibição de normas na tela de pesquisa'

--- src/components/NormaPreview.jsx
+++ src/components/NormaPreview.jsx
@@ -1,29 +1,29 @@
 import { Link } from "react-router";
 
-export default function NormaPreview() {
+export default function NormaPreview(props) {
   return (
-    <div className="grid grid-cols-3 gap-5 mt-10 w-[70%]">
-      <div className="bg-amber-50 h-60 rounded-2xl p-5">
-        <div className="flex flex-col">
-          <div className="flex justify-between">
-            <h2 className="text-sm w-fit px-2 rounded-sm bg-blue-200 border border-blue-400 text-blue-500">
-              Código
-            </h2>
-            <p className="text-green-500">status</p>
-          </div>
-          <h1 className="text-2xl font-bold mt-2">Título</h1>
+    <div className="bg-amber-50 h-60 rounded-2xl p-5">
+      <div className="flex flex-col">
+        <div className="flex justify-between">
+          <h2 className="text-sm w-fit px-2 rounded-sm bg-blue-200 border border-blue-400 text-blue-500">
+            {props.codigo}
+          </h2>
+          <p className="text-green-500">{props.status}</p>
         </div>
-        <div className="flex gap-1">
-          <p className="text-gray-600">Área</p>
-          <p>|</p>
-          <p className="text-gray-400">Última atualização: 01/02/2026</p>
-        </div>
-        <Link to="/visualizarNorma">
-          <button className="w-full bg-gray-200 mt-5 px-4 py-2 rounded-md text-gray-700 font-light text-lg cursor-pointer transition duration-1000 ease-in-out hover:bg-gray-300">
-            Visualizar Norma
-          </button>
-        </Link>
+        <h1 className="text-2xl font-bold mt-2">{props.titulo}</h1>
       </div>
+      <div className="flex gap-1">
+        <p className="text-gray-600">Área</p>
+        <p>|</p>
+        <p className="text-gray-400">
+          Última atualização: {props.dataPublicacao}
+        </p>
+      </div>
+      <Link to="/visualizarNorma">
+        <button className="w-full bg-gray-200 mt-5 px-4 py-2 rounded-md text-gray-700 font-light text-lg cursor-pointer transition duration-1000 ease-in-out hover:bg-gray-300">
+          Visualizar Norma
+        </button>
+      </Link>
     </div>
   );
 }
--- src/pages/PesquisarNorma.jsx
+++ src/pages/PesquisarNorma.jsx
@@ -1,13 +1,50 @@
 import FiltroArea from "../components/FiltroArea";
 import BarraPesquisa from "../components/BarraPesquisa";
 import NormaPreview from "../components/NormaPreview";
+import { useEffect, useState } from "react";
 
 export default function PesquisarNorma() {
+  const [normas, setNormas] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/normas/true`)
+      .then((response) => response.json())
+      .then((data) =>
+        setNormas(
+          data.map((d) => ({
+            id: d.id,
+            codigo: d.codigo,
+            titulo: d.titulo,
+            area_tecnica: d.area_tecnica,
+            orgao_emissor: d.orgao_emissor,
+            versaos: d.versaos.map((v) => ({
+              data_publicacao: v.data_publicacao,
+              status: v.status,
+            })),
+          })),
+        ),
+      );
+  }, []);
+
   return (
     <div className="overflow-y-auto w-[90%] flex flex-col items-center gap-5 px-2">
       <FiltroArea />
       <BarraPesquisa />
-      <NormaPreview />
+
+      <div className="grid grid-cols-3 gap-5 mt-10 w-[70%]">
+        {normas.map((norma) => {
+          const versaoAtiva = norma.versaos.find((v) => v.status);
+          return (
+            <NormaPreview
+              key={norma.id}
+              codigo={norma.codigo}
+              titulo={norma.titulo}
+              status={versaoAtiva?.status ? "norma ativa" : "norma inativa"}
+              dataPublicacao={new Date (versaoAtiva?.data_publicacao).toLocaleDateString()}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
