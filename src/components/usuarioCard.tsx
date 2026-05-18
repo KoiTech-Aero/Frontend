cadbeb8cc9cfc04f1aaa64bd167ca910b0b5a314
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2026-05-18 19:00:57 -0300

    style: responsive usuarioCard

--- src/components/usuarioCard.tsx
+++ src/components/usuarioCard.tsx
@@ -3,7 +3,11 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
-export default function UsuarioCard() {
+interface UsuarioCardProps {
+	open: boolean;
+}
+
+export default function UsuarioCard({ open }: UsuarioCardProps) {
 	const { usuario, logout } = useContext(AuthContext);
 	const navigate = useNavigate();
 
@@ -12,17 +16,20 @@ export default function UsuarioCard() {
 		navigate("/login");
 	}
 
-	const btnLogout =
-		"w-fit h-10 mt-5 flex items-center gap-2 cursor-pointer bg-red-500 p-4 rounded-xl text-white font-bold transition duration-300 hover:bg-red-600";
+	const btnLogout = `w-fit h-10 mt-5 flex items-center gap-2 cursor-pointer  ${open && "bg-red-500 p-4"} rounded-xl text-white font-bold transition duration-300 hover:bg-red-600`;
 
 	return (
-		<div className="absolute bottom-0 p-6 rounded-xl text-white">
-			<p className="font-bold">{usuario?.nome}</p>
-			<p className="text-sm opacity-70">{usuario?.role}</p>
+		<div className={`mt-auto ${open ? " p-6" : " p-3"} rounded-xl text-white`}>
+			{open && (
+				<>
+					<p className="font-bold">{usuario?.nome}</p>
+					<p className="text-sm opacity-70">{usuario?.role}</p>
+				</>
+			)}
 
 			<button type="button" onClick={handleLogout} className={btnLogout}>
 				<LogOut />
-				Sair
+				{open && "Sair"}
 			</button>
 		</div>
 	);
